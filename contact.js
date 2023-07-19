9d9d8f75a9cba24a93887f42413b49939d274fce
JKMENSON <jonahmensonit@gmail.com>
2023-07-19 06:02:59 -0700

    updated

--- contact.js
+++ contact.js
@@ -1,30 +1,31 @@
+
 (function() {
-  // Add your EmailJS User ID, Service ID, and template ID here
-  emailjs.init('YOUR_USER_ID');
-  
+  // Add your EmailJS service ID and template ID here
+  emailjs.init('85pA4X-OlLgxagPQH');
+
   // Attach an event listener to the form submit button
   document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-  
+
     // Get the values from the form
-    const name = document.getElementById('name').value;
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
     const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
+    const country = document.getElementById('country').value;
     const message = document.getElementById('message').value;
-  
+
     // Send the email using EmailJS
-    emailjs.send('YOUR_SERVICE_ID','YOUR_TEMPLATE_ID',{
-      'from_name': name,
+    emailjs.send('service_pt3cyce', 'template_uenngim', {
+      'from_name': firstName + ' ' + lastName,
       'email': email,
-      'phone': phone,
+      'country': country,
       'message': message
     })
       .then(function(response) {
         console.log('Email sent', response.status, response.text);
         alert('Thank you for contacting us!');
         document.getElementById('contact-form').reset();
-      })
-      .catch(function(error) {
+      }, function(error) {
         console.log('Email failed to send', error);
         alert('Sorry, there was an error sending your message. Please try again later.');
       });
